2dd9b344865814e6ce7050170a681c8a68c46a63
GannaKov <zlatta2000@gmail.com>
2023-04-27 01:05:46 +0200

    fetchUsers

--- src/pages/TweetsPage/TweetsPage.jsx
+++ src/pages/TweetsPage/TweetsPage.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TweetCard from "../../components/TweetCard";
+import axios from "axios";
+
+export const instanceBacEnd = axios.create({
+  baseURL: "https://6449944db88a78a8f00b5309.mockapi.io",
+});
+
 export default function TweetsPage() {
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const { data } = await instanceBacEnd.get(`/users`);
+        console.log(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchUsers();
+  }, []);
   return (
     <div>
       <TweetCard />
